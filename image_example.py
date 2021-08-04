ad21759080baf581ff53b435323c266f07098b7d
Ihor Litvinov <ihorlitvinoff@gmail.com>
2021-08-04 17:52:30 +0300

    2nd commit

--- image_example.py
+++ image_example.py
@@ -3,35 +3,40 @@ import sys
 import threading
 
 import cv2
+import queue
 
 
-def handle_img(name, img):
-    if img is None:
+def handle_img(img_config, img_queue):
+    pass
+    if img_config[0] is None:
         sys.exit("Could not read the image.")
-    cv2.namedWindow(f'{name}', cv2.WINDOW_NORMAL)
-    cv2.imshow(f'{name}', img)
+    cv2.namedWindow(f'{img_config[1]}', cv2.WINDOW_NORMAL)
 
-    while cv2.getWindowProperty(f'{name}', cv2.WND_PROP_VISIBLE) >= 1:
+    if cv2.getWindowProperty(f'{img_config[1]}', cv2.WND_PROP_VISIBLE) >= 1:
+        start_point = (0, 0)
+        end_point = (img_config[0].shape[0]-1, img_config[0].shape[1]-1)
+
+        cv2.rectangle(img_config[0], start_point, end_point, (0, 0, 255), thickness=10, lineType=cv2.LINE_8)
+
         k = cv2.waitKey(1)
 
         if k == ord("s"):
-            cv2.imwrite(f"legion{name}.jpg", img)
-            break
+            cv2.imwrite(f"legion{img_config[1]}.jpg", img_config[0])
+
+        img_queue.put(img_config[0])
 
 
-img_tuple = (cv2.imread("/home/i_litvinov/Pictures/legion.jpg", cv2.IMREAD_COLOR),
-             cv2.imread("/home/i_litvinov/Pictures/legion.jpg", cv2.IMREAD_GRAYSCALE),
-             cv2.imread("/home/i_litvinov/Pictures/legion.jpg", cv2.IMREAD_UNCHANGED))
+img_tuple = ((cv2.imread("/home/i_litvinov/Pictures/10.jpg", cv2.IMREAD_COLOR),1),
+             (cv2.imread("/home/i_litvinov/Pictures/10.jpg", cv2.IMREAD_GRAYSCALE),2),
+             (cv2.imread("/home/i_litvinov/Pictures/10.jpg", cv2.IMREAD_UNCHANGED),3))
 
+img_queue = queue.Queue()
 
-with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
-    i = 1
-    futures = []
-    for img in img_tuple:
-        futures.append(executor.submit(handle_img, i, img))
-        i += 1
+for img_config in img_tuple:
+    img_queue.put(img_config)
 
-    for future in futures:
-        future.result()
+while not img_queue.empty():
+    img_config = img_queue.get()
+    handle_img(img_config,img_queue)
 
 cv2.destroyAllWindows()
--- video_from_file.py
+++ video_from_file.py
@@ -1,16 +1,39 @@
-import cv2 as cv
+# Create a video capture object, in this case we are reading the video from a file
+import cv2
 
-cap = cv.VideoCapture('/home/i_litvinov/Downloads/Telegram Desktop/IMG_0323.MOV')
-while cap.isOpened():
-    ret, frame = cap.read()
-    # if frame is read correctly ret is True
-    if not ret:
-        print("Can't receive frame (stream end?). Exiting ...")
+vid_capture = cv2.VideoCapture('/home/i_litvinov/Downloads/Telegram Desktop/IMG_0323.MOV')
+
+if (vid_capture.isOpened() == False):
+    print("Error opening the video file")
+# Read fps and frame count
+else:
+    # Get frame rate information
+    # You can replace 5 with CAP_PROP_FPS as well, they are enumerations
+    fps = vid_capture.get(5)
+    print('Frames per second : ', fps, 'FPS')
+
+    # Get frame count
+    # You can replace 7 with CAP_PROP_FRAME_COUNT as well, they are enumerations
+    frame_count = vid_capture.get(7)
+    print('Frame count : ', frame_count)
+
+while (vid_capture.isOpened()):
+
+    # vid_capture.read() methods returns a tuple, first element is a bool
+    # and the second is frame
+    ret, frame = vid_capture.read()
+
+    # 20 is in milliseconds, try to increase the value, say 50 and observe
+    key = cv2.waitKey(500)
+
+    if key == ord('q') or cv2.getWindowProperty('Frame', cv2.WND_PROP_VISIBLE) < 1:
         break
-    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    cv.namedWindow('frame', cv.WINDOW_NORMAL)
-    cv.imshow('frame', gray)
-    if cv.waitKey(1) == ord('q'):
-        break
-cap.release()
-cv.destroyAllWindows()
+
+    if ret:
+        cv2.namedWindow('Frame', cv2.WINDOW_NORMAL)
+        cv2.imshow('Frame', frame)
+
+
+# Release the video capture object
+vid_capture.release()
+cv2.destroyAllWindows()
