ece4599bbf37aac7208056a1e5d4dddefddb1701
Ihor Litvinov <ihorlitvinoff@gmail.com>
2021-08-05 09:47:21 +0300

    different independent windows - implemented

--- image_example.py
+++ image_example.py
@@ -1,34 +1,34 @@
-import concurrent.futures
+import queue
 import sys
-import threading
 
 import cv2
-import queue
 
 
 def handle_img(img_config, img_queue):
-    pass
     if img_config[0] is None:
         sys.exit("Could not read the image.")
     cv2.namedWindow(f'{img_config[1]}', cv2.WINDOW_NORMAL)
 
     if cv2.getWindowProperty(f'{img_config[1]}', cv2.WND_PROP_VISIBLE) >= 1:
+        buffer_img = img_config[0].copy()
         start_point = (0, 0)
-        end_point = (img_config[0].shape[0]-1, img_config[0].shape[1]-1)
+        thickness = 10
+        end_point = (buffer_img.shape[1], buffer_img.shape[0])
 
-        cv2.rectangle(img_config[0], start_point, end_point, (0, 0, 255), thickness=10, lineType=cv2.LINE_8)
+        cv2.rectangle(buffer_img, start_point, end_point, (0, 255, 255), thickness=thickness, lineType=cv2.LINE_8)
 
+        cv2.imshow(f'{img_config[1]}', buffer_img)
         k = cv2.waitKey(1)
 
         if k == ord("s"):
             cv2.imwrite(f"legion{img_config[1]}.jpg", img_config[0])
 
-        img_queue.put(img_config[0])
+        img_queue.put(img_config)
 
 
-img_tuple = ((cv2.imread("/home/i_litvinov/Pictures/10.jpg", cv2.IMREAD_COLOR),1),
-             (cv2.imread("/home/i_litvinov/Pictures/10.jpg", cv2.IMREAD_GRAYSCALE),2),
-             (cv2.imread("/home/i_litvinov/Pictures/10.jpg", cv2.IMREAD_UNCHANGED),3))
+img_tuple = ((cv2.imread("/home/i_litvinov/Pictures/10.jpg", cv2.IMREAD_COLOR), 1),
+             (cv2.imread("/home/i_litvinov/Pictures/10.jpg", cv2.IMREAD_GRAYSCALE), 2),
+             (cv2.imread("/home/i_litvinov/Pictures/10.jpg", cv2.IMREAD_UNCHANGED), 3))
 
 img_queue = queue.Queue()
 
@@ -37,6 +37,6 @@ for img_config in img_tuple:
 
 while not img_queue.empty():
     img_config = img_queue.get()
-    handle_img(img_config,img_queue)
+    handle_img(img_config, img_queue)
 
 cv2.destroyAllWindows()
